7598601c3e979fcda56337bc3449a472b19ba337
olgavegh <olgi.vegh@gmail.com>
2024-12-11 21:05:36 +0100

    add addThought function

--- src/AddThoughtForm.js
+++ src/AddThoughtForm.js
@@ -6,9 +6,26 @@ export function AddThoughtForm(props) {
   const handleTextChange = (event) => {
     setText(event.target.value);
   };
+  const handleSubmit = (event) => {
+    //To prevent the form from refreshing the page
+    event.preventDefault();
+    //create a new thought
+    const thought = {
+      id: generateId(),
+      text: text,
+      expiresAt: getNewExpirationTime(),
+    };
+    // don’t want to add empty thoughts to our app
+    if (text.length > 0) {
+      // Pass it to addThought()
+      props.addThought(thought);
+      //Clear the input’s text after adding a new thought
+      setText("");
+    }
+  };
 
   return (
-    <form className="AddThoughtForm">
+    <form onSubmit={handleSubmit} className="AddThoughtForm">
       <input
         type="text"
         aria-label="What's on your mind?"
--- src/App.js
+++ src/App.js
@@ -17,13 +17,17 @@ export default function App() {
     },
   ]);
 
+  const addThought = (thought) => {
+    setThoughts((prev) => [thought, ...prev]);
+  };
+
   return (
     <div className="App">
       <header>
         <h1>Passing Thoughts</h1>
       </header>
       <main>
-        <AddThoughtForm />
+        <AddThoughtForm addThought={addThought} />
         <ul className="thoughts">
           {thoughts.map((thought) => (
             <Thought key={thought.id} thought={thought} />
