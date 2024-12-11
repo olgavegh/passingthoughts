4fb697e1c9f45120f7252ab415015e43037a3f1b
olgavegh <olgi.vegh@gmail.com>
2024-12-11 20:21:21 +0100

    content framework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Passing Thoughts</h1>
       </header>
+      <main>
+        <form className="AddThoughtForm">
+          <input
+            type="text"
+            aria-label="What's on your mind?"
+            placeholder="What's on your mind?"
+          />
+          <input type="submit" value="Add" />
+        </form>
+        <ul className="thoughts">
+          <li className="thought">
+            <button aria-label="Remove thought" className="remove-button">
+              x
+            </button>
+            <div className="text">Thought example</div>
+          </li>
+          <li className="thought">
+            <button aria-label="Remove thought" className="remove-button">
+              x
+            </button>
+            <div className="text">Thought example</div>
+          </li>
+        </ul>
+      </main>
     </div>
   );
-}
-
-export default App;
+};
